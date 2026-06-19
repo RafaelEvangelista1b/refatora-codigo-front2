87ebfa1d486614ecf6d761007194818144ac9247
RafaelEvangelista1b <evangelistafreitasrafael@gmail.com>
2026-06-19 09:04:26 -0300

    Delete assets/js directory

--- assets/js/footer.js
+++
@@ -1,9 +0,0 @@
-let footerBtn = document.querySelector('.footer-bottom .btn-icon');
-
-footerBtn.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: 'smooth'
-    });
-});
